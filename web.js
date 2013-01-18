3c737ca012261735ff2a43e0e679da5f4984fd4f
Todd Nash <nash20@gmail.com>
2013-01-18 22:33:45 +0800

    todd's first impact

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello My Town v2!');
+  response.send('Hello My Town v3! from Todd');
 });
 
 var port = process.env.PORT || 5000;
